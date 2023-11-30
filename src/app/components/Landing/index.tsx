f0f4a412ea06d9b8c450248f3512f79c2777a1c1
MiheretuTesh <se.miheretu.degebassa@gmail.com>
2023-12-01 00:10:48 +0300

    feat: graph working

--- src/app/components/Landing/index.tsx
+++ src/app/components/Landing/index.tsx
@@ -1,7 +1,17 @@
 "use client";
 import React, { ButtonHTMLAttributes } from "react";
 import Button from "../Button/Button";
-// import LineChart from "../LineChart";
+import { AiOutlineInfoCircle } from "react-icons/ai";
+import data from "./sampleChartData";
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 
 interface LandingProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: "primary" | "secondary";
@@ -32,16 +42,59 @@ const Landing: React.FC<LandingProps> = ({
             <Button>Withdraw</Button>
           </div>
         </div>
+        <div className="py-4" style={{ width: "100%" }}>
+          <ResponsiveContainer width="100%" height={300}>
+            <AreaChart
+              width={500}
+              height={200}
+              data={data}
+              syncId="anyId"
+              margin={{
+                top: 10,
+                right: 50,
+                left: 50,
+                bottom: 0,
+              }}
+            >
+              <XAxis dataKey="name" ticks={["Apr 1, 2022", "Apr 30, 2022"]} />
+              <Area
+                type="monotone"
+                dataKey="price"
+                stroke="#FF9462"
+                fill="#ffffff"
+              />
+            </AreaChart>
+          </ResponsiveContainer>
+        </div>
       </div>
-      <div className="w-20  p-2 group rounded-lg">
-        <div className="flex justify-between items-center">
-          <div className="flex items-center space-x-2">
-            <div className="flex flex-col text-black">
-              <span className="sm:text-sm">Available Balance</span>
-              <h1 className="font-bold text-2xl">USD 120,500.00</h1>
-            </div>
-            <Button>Withdraw</Button>
+      <div style={{ flexBasis: "15%" }}>
+        <div className="flex items-center  justify-between p-2 group rounded-lg w-full ">
+          <div className="flex flex-col text-black">
+            <span className="sm:text-sm">Ledge Balance</span>
+            <h1 className="font-bold text-xl">USD 0.00</h1>
           </div>
+          <AiOutlineInfoCircle className="text-gray-500" />
+        </div>
+        <div className="flex items-center  justify-between p-2 group rounded-lg w-full ">
+          <div className="flex flex-col text-black">
+            <span className="sm:text-sm">Total Payout</span>
+            <h1 className="font-bold text-xl">USD 55,080.00</h1>
+          </div>
+          <AiOutlineInfoCircle className="text-gray-500" />
+        </div>
+        <div className="flex items-center  justify-between p-2 group rounded-lg w-full ">
+          <div className="flex flex-col text-black">
+            <span className="sm:text-sm">Total Revenue</span>
+            <h1 className="font-bold text-xl">USD 175,580.00</h1>
+          </div>
+          <AiOutlineInfoCircle className="text-gray-500" />
+        </div>
+        <div className="flex items-center  justify-between p-2 group rounded-lg w-full ">
+          <div className="flex flex-col text-black">
+            <span className="sm:text-sm">Pending Payout</span>
+            <h1 className="font-bold text-xl">USD 0.00</h1>
+          </div>
+          <AiOutlineInfoCircle className="text-gray-500" />
         </div>
       </div>
     </div>
---
+++ src/app/components/Landing/sampleChartData.tsx
@@ -0,0 +1,11 @@
+// sample data for the chart
+const data = [
+  { name: "Apr 1, 2022", price: 400 },
+  { name: "Apr 6, 2022", price: 3000 },
+  { name: "Apr 12, 2022", price: 1000 },
+  { name: "Apr 18, 2022", price: 2780 },
+  { name: "Apr 24, 2022", price: 2290 },
+  { name: "Apr 30, 2022", price: 400 },
+];
+
+export default data;
